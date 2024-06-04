95350c323c5f8948ad8021705f2fa0c9e17baee1
Tsiory RAKOTOSOA <tsioryrakotosoa@gmail.com>
2024-06-04 22:28:27 +0300

    feature(team): Open show team information done.

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,1 +1,8 @@
-console.log('coucou');
+document.addEventListener('DOMContentLoaded', function () {
+  let rows = document.querySelectorAll('.team-row');
+  rows.forEach(function (row) {
+    row.addEventListener('click', function () {
+      window.location.href = row.dataset.href;
+    });
+  });
+});
